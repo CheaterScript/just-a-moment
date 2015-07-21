7715a250b1f9e1d0eb8223d73f181518d79020c1
Andy <361932463@qq.com>
2015-07-21 18:16:33 +0800

    添加A*寻径

--- scene/MainScene.js
+++ scene/MainScene.js
@@ -36,6 +36,7 @@ var game;
     var facing = 'down';
     var bounce;
     var speed = 100;
+    var mapData;
 	function create(){
         map = game.add.tilemap('map');
         map.addTilesetImage('treeA');
@@ -48,9 +49,6 @@ var game;
         map.addTilesetImage('tumian');
         layer = map.createLayer('layer 1');
         layer.resizeWorld();
-        layer2 = map.createLayer('layer 2');
-        layer2.resizeWorld();
-
         player = game.add.sprite(200,200,'hero');
         player.animations.add('down',[0,1,2,3],12,true);
         player.animations.add('left',[4,5,6,7],12,true);
@@ -59,6 +57,9 @@ var game;
         player.anchor.set(0.5,0.9);
         player.isMove = false;
 
+        layer2 = map.createLayer('layer 2');
+        layer2.resizeWorld();
+        mapData = layer.layer.data;
         cursor = game.input.keyboard.createCursorKeys();
         game.camera.follow(player);
         createUI();
@@ -66,31 +67,41 @@ var game;
 
 	function update(){
         if(game.input.activePointer.isDown && !player.isMove){
-            var playerPosition = player.position;
+            var playerPositionX = ~~(player.position.x/80);
+            var playerPositionY = ~~(player.position.y/80);
             var position = getMapPoint(game.input.activePointer.worldX,game.input.activePointer.worldY);
-            var facingX = (position.x - playerPosition.x);
-            facingX && (facingX /= Math.abs(facingX));
-            var facingY = (position.y - playerPosition.y);
-            facingY && (facingY /= Math.abs(facingY));
-            if(facingX || facingY){
-                if(facingX){
-                    player.isMove = true;
-                    var tween = game.add.tween(player).to({x:position.x},speed*Math.abs((position.x - playerPosition.x)/80)).start();
-                    tween.onComplete.add(XEnd.bind(this,facingY,position),this);
-                }
-                else{
-
-                }
+            var endX = ~~(position.x/80);
+            var endY = ~~(position.y/80);
+            if ((playerPositionX != endX || playerPositionY != endY)&&mapData[endY][endX].index!=5){
+                var path = findPath({x:playerPositionX,y:playerPositionY},{x:endX,y:endY});
+                var node = path.pop();
+                player.isMove = true;
+                var tween = game.add.tween(player).to({x:node.x*80+40,y:node.y*80+40},speed).start();
+                tween.onComplete.add(XEnd.bind(this,position,path),this);
             }
+            // var facingX = (position.x - playerPosition.x);
+            // facingX && (facingX /= Math.abs(facingX));
+            // var facingY = (position.y - playerPosition.y);
+            // facingY && (facingY /= Math.abs(facingY));
+            // if(facingX || facingY){
+            //     if(facingX){
+            //         player.isMove = true;
+            //     }
+            //     else{
+
+            //     }
+            // }
         }
 	}
 
-    function XEnd(facingY,position){
-        player.isMove = false;
-
-    }
-
-    function YEnd(){
+    function XEnd(position,path){
+        if(path.length <= 0){
+            player.isMove = false;
+            return;
+        }
+        var node = path.pop();
+        var tween = game.add.tween(player).to({x:node.x*80+40,y:node.y*80+40},speed).start();
+        tween.onComplete.add(XEnd.bind(this,position,path),this);
 
     }
 
@@ -120,6 +131,137 @@ var game;
         thirstyBar.fixedToCamera = true;
         var repeatBar = game.add.sprite(barX,90,'repeat');
         repeatBar.fixedToCamera = true;
+    }
+
+    function findPath(start,end){
+        var point = function(obj){
+            this.x = obj.x;
+            this.y = obj.y;
+            this.f = 0;
+            this.g = 0;
+            this.h = 0;
+            this.inClose = false;
+            this.parent = null;
+        }
+        point.prototype = {
+            countF:function(){
+                this.f = this.h + this.g;
+            }
+        }
+
+        var openList = [];
+        var closeList = [];
+        var findPoint = function(x,y,obj){
+            for(var key in obj){
+                if(obj[key].x == x&&obj[key].y == y)return obj[key];
+            }
+            return false;
+        }
+        var start = new point(start);
+        var end = new point(end);
+        openList.push(start);
+        while(openList.length > 0){
+            openList.sort(compare);
+            var parent = openList.shift(); 
+            closeList.push(parent);
+            if(parent.x == end.x && parent.y == end.y){
+                break;
+            }
+            var x = parent.x;
+            var y = parent.y;
+            if (x<128&&y-1<72&&mapData[y-1][x].index!=5&&!findPoint(x,y-1,closeList)) {
+                var p = findPoint(x,y-1,openList)
+                if (p) {
+                    var g = Math.abs(start.x-p.x)+Math.abs(start.y-p.y);
+                    if (g < p.g) {
+                        p.g = g;
+                        p.countF();
+                        p.parent = parent;
+                    }
+
+                }
+                else{
+                    p = new point({x:x,y:y-1});
+                    p.g = parent.g+1;
+                    p.h = Math.abs(end.x - x)+Math.abs(end.y-(y-1));
+                    p.countF();
+                    p.parent = parent;
+                    openList.push(p);
+                }
+            }
+            if (x<128&&y+1<72&&mapData[y+1][x].index!=5&&!findPoint(x,y+1,closeList)) {
+                var p = findPoint(x,y+1,openList)
+                if (p) {
+                    var g = Math.abs(start.x-p.x)+Math.abs(start.y-p.y);
+                    if (g < p.g) {
+                        p.g = g;
+                        p.countF();
+                        p.parent = parent;
+                    }
+
+                }
+                else{
+                    p = new point({x:x,y:y+1});
+                    p.g = parent.g+1;
+                    p.h = Math.abs(end.x - x)+Math.abs(end.y-(y+1));
+                    p.countF();
+                    p.parent = parent;
+                    openList.push(p);
+                }
+            }
+            if (x-1<128&&y<72&&mapData[y][x-1].index!=5&&!findPoint(x-1,y,closeList)) {
+                var p = findPoint(x-1,y,openList)
+                if (p) {
+                    var g = Math.abs(start.x-p.x)+Math.abs(start.y-p.y);
+                    if (g < p.g) {
+                        p.g = g;
+                        p.countF();
+                        p.parent = parent;
+                    }
+
+                }
+                else{
+                    p = new point({x:x-1,y:y});
+                    p.g = parent.g+1;
+                    p.h = Math.abs(end.x - (x-1))+Math.abs(end.y-y);
+                    p.countF();
+                    p.parent = parent;
+                    openList.push(p);
+                }
+            }
+            if (x+1<128&&y<72&&mapData[y][x+1].index!=5&&!findPoint(x+1,y,closeList)) {
+                var p = findPoint(x+1,y,openList)
+                if (p) {
+                    var g = Math.abs(start.x-p.x)+Math.abs(start.y-p.y);
+                    if (g < p.g) {
+                        p.g = g;
+                        p.countF();
+                        p.parent = parent;
+                    }
+
+                }
+                else{
+                    p = new point({x:x+1,y:y});
+                    p.g = parent.g+1;
+                    p.h = Math.abs(end.x - (x+1))+Math.abs(end.y-y);
+                    p.countF();
+                    p.parent = parent;
+                    openList.push(p);
+                }
+            }
+
+        }
+        var node = closeList.pop();
+        var path = [];
+        while(node.x != start.x || node.y != start.y){
+            path.push(node);
+            node = node.parent;
+        }
+        return path;
+    }
+
+    function compare(a,b){
+        return a.f - b.f;
     }
 
 })()
